19af7e00fac471c40cf9c9fbb1059c9df1a02655
Qinxin Zhang <44661616+qinxinzhang@users.noreply.github.com>
2024-09-02 17:02:17 +0800

    add angular service

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,21 +1,26 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import {RouterModule} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-housing-location',
+  imports:[RouterModule],
   template: `
-    <section class="listing">
-      <img
+  <main>
+    <a [routerLink]="['/details', housingLocation.id]">
+      <section class="listing">
+        <img
         class="listing-photo"
         [src]="housingLocation.photo"
         alt="Exterior photo of {{ housingLocation.name }}"
-        crossorigin
-      />
+        crossorigin/>
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
-    </section>
+      </section>
+    </a>
+  </main>
+
 `,
   styleUrls: ['./housing-location.component.css']
 })
